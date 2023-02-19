950b6ded2bfe1459858b903668516ccf20f14e9a
Kevin <marquezberriosk@gmail.com>
2023-02-18 22:17:50 -0400

    Arreglo método disparo de CannonBall1

--- src/powerups/CannonBall1.py
+++ src/powerups/CannonBall1.py
@@ -30,6 +30,7 @@ class CannonBall1(PowerUp):
         super().__init__(x, y, 5)
         self.activate = False
         self.shoots = 1
+        self.projectiles = []
         self.ball_factory = Factory(Ball)
         self.texture_cannons = settings.TEXTURES["cannons"]
         self.frames_cannons = settings.FRAMES["cannons"]
@@ -38,19 +39,25 @@ class CannonBall1(PowerUp):
         paddle = play_state.paddle
         paddle.cannon = True
         self.activate = True
+        self.projectiles = []
         self.in_play = False
-        for _ in range(2):
-            b = self.ball_factory.create(paddle.x, paddle.y - 8)
-            b.vx = 0
-            b.vy = -256
-            play_state.projectiles.append(b)
 
     def is_active(self) -> bool:
         return self.activate
     
-    def shoot(self) -> None:
+    def shoot(self, play_state: TypeVar("PlayState")) -> None:
         if self.shoots > 0:
             self.shoots -= 1
+            for _ in range(2):
+                b = self.ball_factory.create(play_state.paddle.x, play_state.paddle.y - 8)
+                b.vx = 0
+                b.vy = -256
+                self.projectiles.append(b)
+            
+            self.projectiles[0].x = play_state.paddle.x + play_state.paddle.width + 3
+            self.projectiles[1].x = play_state.paddle.x - 14
+            play_state.balls.extend(self.projectiles)
+            self.projectiles = []
         else:
             self.activate = False
 
